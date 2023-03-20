6ea10a2f05001f2435b26b7f33209103f8a864e0
Knarkzel <knarkzel@gmail.com>
2023-03-20 02:25:01 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    esp32 = {
+      url = "github:knarkzel/esp32";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    esp32,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+    idf-rust = esp32.packages.x86_64-linux.esp32;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [
+        idf-rust
+      ];
+
+      shellHook = ''
+        export RUSTUP_TOOLCHAIN="esp"
+        export RUSTUP_HOME="${idf-rust}/.rustup"
+        export PATH="${idf-rust}/.rustup/toolchains/esp/bin:$PATH"
+        export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+      '';
+    };
+  };
+}
